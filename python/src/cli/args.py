955997a19aa0fee080cfc14043e87a30b62ab436
vigenere23 <lolgab1@hotmail.com>
2021-10-29 10:16:11 -0400

    added cli help messages

--- python/src/cli/args.py
+++ python/src/cli/args.py
@@ -3,11 +3,11 @@ from tap import Tap
 
 
 class DemoArgs(Tap):
-    serial_port: str
-    channels: List[int]
-    plot: bool = False
-    csv: bool = False
+    serial_port: str # write 'fake' for a fake one
+    channels: List[int] # channels to use for plotting
+    plot: bool = False # plot selected channels
+    csv: bool = False # save all data to a CSV
 
 
 class SpeedTestArgs(Tap):
-    serial_port: str
+    serial_port: str # write 'fake' for a fake one
